b80d26f14b5cfb8c50494b864a2ffecf1353ae2a
Joshua Evuetapha <Evuetaphajoshua@gmail.com>
2023-10-22 16:51:48 +0100

    feat: more features achievement

--- rps-onchain/src/pages/achievements/index.tsx
+++ rps-onchain/src/pages/achievements/index.tsx
@@ -2,10 +2,12 @@ import AchievementCard from "@/components/utils/AchievementCard";
 import Container from "@/components/utils/Container";
 import Layout from "@/components/utils/Layout";
 import useContractAddr from "@/hooks/useContractAddr";
-import { useChainId, useContractRead } from "wagmi";
+import { useAccount, useChainId, useContractRead } from "wagmi";
 import RPSGameFactory from "@/abi/contracts/src/RPSGameFactory.sol/RPSGameFactory.json";
 
 export default function Achievement() {
+
+  const { address } = useAccount()
 
   const contract = useContractAddr()
 
@@ -18,8 +20,17 @@ export default function Achievement() {
     chainId: chainId
   })
 
+  const gamerProfile = useContractRead({
+    address: contract,
+    abi: RPSGameFactory,
+    functionName: 'gamerProfile',
+    chainId: chainId,
+    args: [address]
+  })
 
- console.log(achievementManagerAddress)
+  console.log(gamerProfile)
+
+  const wins = (gamerProfile?.data as any)?.[2]
 
   return (
     <Layout>
@@ -28,23 +39,23 @@ export default function Achievement() {
 
         <div className='flex flex-grow flex-col gap-5 justify-center items-center text-white w-full my-20'>
           
-          <AchievementCard achievementType="rps-br" manager={achievementManagerAddress.data} />
+          <AchievementCard uniqueVictories={wins} achievementType="rps-br" manager={achievementManagerAddress.data} />
 
-          <AchievementCard achievementType="rps-f" manager={achievementManagerAddress.data}/>
+          <AchievementCard uniqueVictories={wins} achievementType="rps-f" manager={achievementManagerAddress.data}/>
 
-          <AchievementCard achievementType="rps-s" manager={achievementManagerAddress.data}/>
+          <AchievementCard uniqueVictories={wins} achievementType="rps-s" manager={achievementManagerAddress.data}/>
 
-          <AchievementCard achievementType="rps-w" manager={achievementManagerAddress.data}/>
+          <AchievementCard uniqueVictories={wins} achievementType="rps-w" manager={achievementManagerAddress.data}/>
 
-          <AchievementCard achievementType="rps-sh" manager={achievementManagerAddress.data}/>
+          <AchievementCard uniqueVictories={wins} achievementType="rps-sh" manager={achievementManagerAddress.data}/>
 
-          <AchievementCard achievementType="rps-b" manager={achievementManagerAddress.data}/>
+          <AchievementCard uniqueVictories={wins} achievementType="rps-b" manager={achievementManagerAddress.data}/>
 
-          <AchievementCard achievementType="rps-gen" manager={achievementManagerAddress.data} />
+          <AchievementCard uniqueVictories={wins} achievementType="rps-gen" manager={achievementManagerAddress.data} />
 
-          <AchievementCard achievementType="rps-war" manager={achievementManagerAddress.data} />
+          <AchievementCard uniqueVictories={wins} achievementType="rps-war" manager={achievementManagerAddress.data} />
 
-          <AchievementCard achievementType="rps-king" manager={achievementManagerAddress.data} />
+          <AchievementCard uniqueVictories={wins} achievementType="rps-king" manager={achievementManagerAddress.data} />
         
         </div>
         
